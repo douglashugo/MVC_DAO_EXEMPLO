4dbebf302e55900064019c23f194c34e61e5e14f
Esdras B Silva <bzr4@icloud.com>
2023-04-28 20:06:41 -0300

    Remocao codigo do Main

--- src/App.java
+++ src/App.java
@@ -1,23 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-        String url = "jdbc:mysql://localhost:3306/fatec";
-        String user = "root";
-        String passwd = "segredo";
-
-        ResultSet rst = DriverManager
-                .getConnection(url, user, passwd)
-                .createStatement()
-                .executeQuery("select version() as versao;");
-
-        if(rst.next()) {
-            String versao = rst.getString("versao");
-            System.out.printf("Versão: %s\n", versao);
-        }
-
+        System.out.println("Testing...");
     }
 }
